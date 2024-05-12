607670e925c90e0d34e409ee7959a3aa57060a4f
elifoguzkan <oguzkanelif@gmail.com>
2024-05-12 19:28:06 +0300

    first commit

---
+++ finding-ip-address/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace finding_ip_address
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(40, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1089, 24);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(40, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1089, 24);
+            this.textBox2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(40, 101);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(421, 324);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(502, 101);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(627, 324);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(40, 442);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(1089, 324);
+            this.listBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 787);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ finding-ip-address/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.ServiceProcess;
+using System.Net.NetworkInformation;
+namespace finding_ip_address
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.Columns.Add("Servis Adı", 180);
+            listView1.Columns.Add("Görünen Adı", 180);
+            listView1.Columns.Add("Servis Tipi", 180);
+            listView1.Columns.Add("Durum", 70);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string hostName = Dns.GetHostName();
+            string myIp = Dns.GetHostEntry(hostName).AddressList[0].ToString();
+            textBox1.Text = myIp;
+            textBox2.Text = hostName;
+            foreach(ServiceController service in ServiceController.GetServices())
+            {
+                string serviceName = service.ServiceName;
+                string serviceDisplayName = service.DisplayName;
+                string serviceType = service.ServiceType.ToString();
+                string status = service.Status.ToString();
+                listBox1.Items.Add(serviceName + " " + serviceDisplayName + serviceType + " " + status);
+                ListViewItem item = new ListViewItem(serviceName);
+                item.SubItems.Add(serviceDisplayName);
+                item.SubItems.Add(serviceType);
+                item.SubItems.Add(status);
+                listView1.Items.Add(item);
+                ListTcpPorts();
+            }
+        }
+        private void ListTcpPorts()
+        {
+            // Mevcut TCP bağlantılarını ve dinleyicilerini al
+            var tcpConnections = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpConnections();
+            var tcpListeners = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners();
+
+            // TCP bağlantılarını listele
+            foreach (var connection in tcpConnections)
+            {
+                string localEndpoint = $"{connection.LocalEndPoint.Address}:{connection.LocalEndPoint.Port}";
+                string remoteEndpoint = $"{connection.RemoteEndPoint.Address}:{connection.RemoteEndPoint.Port}";
+                string state = connection.State.ToString();
+                listBox2.Items.Add($"Bağlantı: {localEndpoint} -> {remoteEndpoint} ({state})");
+            }
+            // TCP dinleyicilerini listele
+            foreach (var listener in tcpListeners)
+            {
+                string localEndpoint = $"{listener.Address}:{listener.Port}";
+                listBox2.Items.Add($"Dinleyici: {localEndpoint}");
+            }
+        }
+    }
+}
